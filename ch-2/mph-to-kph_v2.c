f4edeafb954977f9c5046033f3d0b8128e106127
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 15:36:01 +0300

    Adding code ...

--- ch-2/mph-to-kph_v2.c
+++ ch-2/mph-to-kph_v2.c
@@ -4,3 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+void printHelp(FILE *stream, char progname[]);
+
+int main(int argc, char* argv[]) {
+    char mph[10] = { 0 };
+    int opt;
+    int cont = 0;
+
+    // parse the command-line options
+    while((opt = getopt(argc, argv, "ch")) != -1) {
+        switch (opt) {
+            case 'h':
+                printHelp(stdout, argv[0]);
+                break;
+            case 'c':
+                cont = 1;
+                break;
+            default:
+                printHelp(stderr, argv[0]);
+                return 1;
+        }
+    }
+
+    while(fgets(mph, sizeof(mph), stdin) != NULL) {
+        if(strspn(mph, "0123456789.-\n") == strlen(mph)) {
+            
+        }
+    }
+
+    return 0;
+}
