4a3b860fcd6ac27e44f6d5b1b6db4184d325d1fe
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 13:58:39 +0300

    Added a comment

--- ch-2/piping-demo.sh
+++ ch-2/piping-demo.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# this is an example of piping linux shell commands
 # list all the symbolic links in the root directory
 
 ls -l / | grep lrwx | awk '{ print $9 }' | sed 's/^/This is a link : /'
