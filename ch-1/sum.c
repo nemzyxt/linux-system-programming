7bf61e58aa3740e06ed194309611350e3b0fa4fa
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 10:33:45 +0300

    Wrote code for sum.c

---
+++ ch-1/sum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printhelp(char progname[]);
+
+int main(int argc, char* argv[]) {
+    int i, sum = 0;
+
+    if(argc == 1) {
+        printhelp(argv[0]);
+        return 1;
+    }
+
+    for(i=0; i<argc; i++) {
+        sum += argv[i];
+    }
+
+    printf("Total sum : %d\n", sum);
+    return 0;
+}
+
+void printhelp(char progname[]) {
+    printf("%s integer ... \n", progname);
+    printf("This program takes any number of integer values"
+           " as arguments and sums them up");
+}
