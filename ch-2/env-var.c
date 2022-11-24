7ed17a8efbdd19f59fc3bdcfc54b1a72794c3a54
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 16:01:09 +0300

    Added the code

--- ch-2/env-var.c
+++ ch-2/env-var.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main(void) {
+    // print out some environment variables : getenv() function
+    printf("Your username is : %s\n", getenv("USER"));
+    printf("Your home directory is : %s\n", getenv("HOME"));
+    printf("Your preferred editor is : %s\n", getenv("EDITOR"));
+    printf("Your shell is : %s\n", getenv("SHELL"));
+
+    // check if the current terminal supports colors
+    if(strstr(getenv("TERM"), "256color")) {
+        printf("\033[0;31mYour \033[0;32mterminal "
+               "\033[0;33msupports \033[0;34mcolors"
+               "\033[0m\n");
+    } else {
+        printf("Your terminal doesn't support colors\n");
+    }
+
+    return 0;
+}
