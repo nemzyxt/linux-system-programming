32688087fe49d22a2445497ebaf0c315ee93ff69
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 14:08:50 +0300

    Added comments

--- ch-2/output.c
+++ ch-2/output.c
@@ -6,6 +6,8 @@ int main(void) {
     printf("A regular message on stdout\n");
     fprintf(stdout, "Also a regular message on stdout\n");
 
+    // using file descriptors,
+    // which requires the _POSIX_C_SOURCE 200809L
     dprintf(1, "A regular message, printed to fd 1\n");
     dprintf(2, "An error message, printed to fd 2\n");
 
