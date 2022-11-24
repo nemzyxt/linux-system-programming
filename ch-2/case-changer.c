f6cd1c47fb09bb94c7a963ccdc2322dc1799f7d7
Nemzyxt <bR0S3@protonmail.com>
2022-11-24 14:31:07 +0300

    Added the code

---
+++ ch-2/case-changer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char c[20] = { 0 };
+    char newcase[20] = { 0 };
+    int i;
+
+    while(fgets(c, sizeof(c), stdin) != NULL) {
+        for(i=0; i<sizeof(c); i++) {
+            if((c[i]>=65) && (c[i]<=90)) {
+                newcase[i] = c[i] + 32;
+            } 
+
+            if((c[i]>=97) && c[i]<=122) {
+                newcase[i] = c[i] - 32;
+            }
+        }
+    }
+
+    printf("%s", newcase);
+
+    memset(c, 0, sizeof(c));
+    memset(newcase, 0, sizeof(newcase));
+}
