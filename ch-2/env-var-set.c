ea3f79bab04f9e5b42817fab11fee1856b097b98
Nemuel Wainaina <bR0S3@protonmail.com>
2023-08-21 12:52:37 +0300

    Create and set environment variables

---
+++ ch-2/env-var-set.c
@@ -0,0 +1,9 @@
+#define _POSIX_C_SOURCE 200112L
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    setenv("FULLNAME", "John-Doe", 1);
+    printf("Your full name is %s\n", getenv("FULLNAME"));
+    return 0;
+}
